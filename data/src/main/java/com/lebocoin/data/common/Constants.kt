38fed532be4acf6b9576bc73edfc645aa09b0cb8
Josue Marinero <josue.marinero@gmail.com>
2025-02-10 11:10:04 +0100

    solve problem showing images

--- data/src/main/java/com/lebocoin/data/common/Constants.kt
+++ data/src/main/java/com/lebocoin/data/common/Constants.kt
@@ -1,3 +1,5 @@
 package com.lebocoin.data.common
 
 internal const val BASE_URL = "https://static.leboncoin.fr/"
+const val IMAGE_BASE_URL = "https://place-hold.it/"
+const val IMAGE_OLD_URL = "https://via.placeholder.com/"
